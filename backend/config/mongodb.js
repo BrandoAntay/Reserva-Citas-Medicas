0cb35cdace176582ebbcf81de307a1b062a2c2b5
Brando <brandoantay29@gmail.com>
2025-05-19 21:00:52 -0500

    Configuracion del almacenamiento Cloud NY con Clave API y clave secreta

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
 
     mongoose.connection.on('connected', () => console.log("Database Connected"))
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/Reserve-Booking`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/Booking`)
 }
 
 export default connectDB
